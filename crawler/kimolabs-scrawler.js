1b12676b7a6480ac4e84915520b2298ac14b9ef5
Fizer Khan <fizerkhan@gmail.com>
2014-12-11 15:16:02 +0530

    updated tamil json.

--- crawler/kimolabs-scrawler.js
+++ crawler/kimolabs-scrawler.js
@@ -1,19 +1,14 @@
 //  This code does not work
 var request = require('request');
-var async = require('async');
+var _ = require('underscore');
 var fs = require('fs');
 var thirukuralData = require('./thirukural.json');
 var kurals = thirukuralData.kurals;
 
-var apiKey = process.env.APIKEY;
-if (!apiKey) {
-  console.log('There is no APIKEY');
-  process.exit();
-}
-
+console.log(__dirname);
 function saveFile() {
   var result = JSON.stringify(thirukuralData, null, 2);
-  fs.writeFile(__dirname + 'data.json', result, function(err) {
+  fs.writeFile(__dirname + '/thirukural.json', result, function(err) {
     if(err) {
       console.log(err);
     } else {
@@ -22,56 +17,52 @@ function saveFile() {
   });
 }
 
-function getTamilMeaning() {
-    var options = {
-        url: 'https://www.kimonolabs.com/api/3b16d948?apikey='+ apiKey,
-        json: true,
-        headers: {
-            'User-Agent': 'request'
-        }
-    };
-    request(options, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-          var array = body.results.thirukural;
-          for (var i = 0; i <= array.length; i++) {
-            var object = array[i];
-            var kural = kurals[i];
-            kural.meaning = kural.meaning || {};
-            kural.meaning.ta_mu_va = object.ta_mu_va;
-            kural.meaning.ta_salamon = object.ta_salamon;
-          };
-          saveFile();
-      } else {
-        console.log('Tamil Error: ' + error);
-      }
-  });
+var tamilArray = require('./thirukural-ta.json');
+var englishArray = require('./thirukural-en.json');
+
+for (var j = 0; j < tamilArray.length; j++) {
+    var object = tamilArray[j];
+    var number = parseInt(object.url.split('=')[1]);
+    var index = number - 1;
+    var kural = kurals[index];
+    kural.meaning = kural.meaning || {};
+    kural.meaning.ta_mu_va = object.ta_mu_va;
+    kural.meaning.ta_salamon = object.ta_salamon;
 }
 
-function getEnglishMeaning() {
-    var options = {
-        url: 'https://www.kimonolabs.com/api/2mfbd70k?apikey='+ apiKey,
-        json: true,
-        headers: {
-            'User-Agent': 'request'
-        }
-    };
-    request(options, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-          var array = body.results.thirukural;
-          for (var i = 0; i <= array.length; i++) {
-            var object = array[i];
-            var kural = kurals[i];
-            kural.meaning = kural.meaning || {};
-            kural.meaning.en = object.en;
-          };
-          saveFile();
-      } else {
-        console.log('English Error: ' + error);
-      }
-  });
+for (var j = 0; j < englishArray.length; j++) {
+    var object = englishArray[j];
+    var number = parseInt(object.url.split('=')[1]);
+    var index = number - 1;
+    var kural = kurals[index];
+    kural.meaning = kural.meaning || {};
+    kural.meaning.en = object.en;
 }
 
-// getTamilMeaning();
-// getEnglishMeaning();
-console.log(require('./thirukural-ta.json').length);
-console.log(require('./thirukural-en.json').length);
+saveFile();
+
+// var kuralsIndexes = [];
+// for (var i = 1; i <= 1330; i++) {
+//     kuralsIndexes.push(i);
+// }
+
+// var availableIndexes = [];
+// for (var i = 0; i < englishArray.length; i++) {
+//     var number = parseInt(englishArray[i].url.split('=')[1]);
+//     availableIndexes.push(number);
+// };
+// availableIndexes = _.uniq(availableIndexes);
+
+// var diffArray = _.difference(kuralsIndexes, availableIndexes);
+
+// console.log('Length ' + kuralsIndexes.length);
+// console.log('Length ' + availableIndexes.length);
+// console.log(diffArray.join(','));
+
+// for (var j = 0; j < diffArray.length; j++) {
+//     // console.log('http://www.dinamalar.com/kural_detail.asp?kural_no=' + diffArray[j]);
+//     console.log('http://www.dinamalar.com/kural_detail_en.asp?kural_no=' + diffArray[j]);
+// }
+
+
+
